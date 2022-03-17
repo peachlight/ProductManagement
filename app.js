f02fe191e85592304503263cf7267ed3be55d026
witatasya <53542676+peachlight@users.noreply.github.com>
2022-03-18 00:48:16 +0700

    First Commit
    Dummy landing page for product management

---
+++ app.js
@@ -0,0 +1,8 @@
+var learnmore_btn = document.querySelector(".content .textBox a");
+const section = document.querySelector("section");
+
+function learnmoreFunc(){
+    alert("Sorry, we are not ready yet! Please wait for further updates!");
+}
+
+learnmore_btn.addEventListener("click", learnmoreFunc);
